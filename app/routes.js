5ba8da3a7604b125aafcb8d5261f5f0cdaae2534
Ethan-McDowall <78203378+Ethan-McDowall@users.noreply.github.com>
2023-03-26 22:02:17 +0100

    updated user journeys and data

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,33 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+router.post('/template-or-role', function (req, res) {
+
+    let templateOrRole = req.session.data['select-a-template-or-from-all-roles']
+  
+    if (templateOrRole === 'omu-case-administrator') {
+      res.redirect('/version-1/select-roles-from-a-template.html')
+    } 
+    else if (templateOrRole === 'omu-manager') {
+        res.redirect('/version-1/select-roles-from-a-template.html')
+    }
+    else if (templateOrRole === 'omu-prison-offender-manager') {
+        res.redirect('/version-1/select-roles-from-a-template.html')
+    }
+    else if (templateOrRole === 'reception-officer') {
+        res.redirect('/version-1/select-roles-from-a-template.html')
+    }
+    else if (templateOrRole === 'security-administrator') {
+        res.redirect('/version-1/select-roles-from-a-template.html')
+    }
+    else if (templateOrRole === 'security-manager') {
+        res.redirect('/version-1/select-roles-from-a-template.html')
+    }
+    else if (templateOrRole === 'wing-officer') {
+        res.redirect('/version-1/select-roles-from-a-template.html')
+    }
+    else {
+        res.redirect('version-1/all-roles')
+    }
+  })
